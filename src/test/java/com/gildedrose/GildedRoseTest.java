7c4d22a8e200114ed9e5e1657333e993b927e4ac
LeSecret <arnaudnjr1998@gmail.com>
2023-09-30 00:32:05 +0300

    test pour voir le changement de qualité chez les articles par defaut

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -14,4 +14,17 @@ class GildedRoseTest {
     assertEquals("foo", element.name, "the name changed");
   }
   
+  @Test
+  @DisplayName("Test pour voir le changement des articles par défaut")
+  void ArticleDefautTest() {
+    Item element1 = new Item("article", 2, 50);
+    Item element2 = new Item("article", 0, 50);
+    Item element3 = new Item("article", -2, 6);
+
+    GildedRose app = new GildedRose(new Item[] {element1, element2, element3});
+    app.updateQuality();
+    assertEquals(49, element1.quality, "La qualité n'a pas été changé");
+    assertEquals(48, element2.quality, "La qualité n'a pas été changé");
+    assertEquals(4, element3.quality, "La qualité n'a pas été changé");
+  }
 }
